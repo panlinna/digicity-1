0c0bbc9f3769470c1d9c8642c3ad11985f98bb58
Peter Wang <happypeter1983@gmail.com>
2016-07-16 16:36:58 +0800

    multi components

--- 006-multi-components/src/index.js
+++ 006-multi-components/src/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import Button from './Button.js';
+import Button from './components/Button.js';
+import Input from './components/Input.js'
 
 
 class App extends Component {
   render(){
     return(
       <div>
-        <Button />
+        <Input />
         <Button />
       </div>
     )
